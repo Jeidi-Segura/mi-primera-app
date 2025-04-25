2e67e508ad765c0db42f76912a7a099f8d6003ac
Jeidi-Segura <alexandraseguraalmario@gmail.com>
2025-04-25 11:24:01 -0500

    Tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,26 @@ function App() {
   return (
     <>
       
-      <div>
+      <div className=' text-white flex flex-col items-center justify-center p-10'>
         <a href="https://www.sena.edu.co/es-co/Paginas/default.aspx" target="_blank">
-          <img src={senaLogo} className="logo" alt="Vite logo" />
+          <img src={senaLogo} className='w-90 mb-6' alt="SENA logo" />
         </a>
-  
       </div>
-
-      
-
-      <h1>Mi primera APP</h1> {count}
-      <div>
+      <h1 className='font-mono && font-bold && text-6xl && text-center && text-white'>Mi primera APP</h1> 
+      <div className='text-white text-center text-5xl pt-24'>
         {/* Usa el componente como una etiqueta HTML */}
-        <Saludo nombre="Jeidi" />
-      </div>  
-      <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
+        <Saludo nombre="Jeidi" /> 
+      </div> 
+      <div className='text-white text-center text-5xl pt-15'>{count} </div>
+
+      <div className="pt-12 text-center">
+          <button className='border-4 border-purple-500 && rounded-xl && outline-offset-4 text-white text-center text-5xl '  onClick={() => setCount((count) => count + 1)}>
             count is
           </button>
       </div>
-      <div className="card1">
+      <div className="pt-12 text-center">
           
-          <button onClick={() => setCount((count) => count - 1)}>
+          <button className='border-4 border-purple-500 && rounded-xl && outline-offset-4 text-white text-center text-5xl' onClick={() => setCount((count) => count - 1)}>
             decrement 
           </button>
       </div>
@@ -38,7 +36,7 @@ function App() {
       <p>
         Edit <code>src/App.jsx</code> and save to test HMR
       </p>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-white text-center text-4xl pt-52">
         Click on the Vite and React logos to learn more
       </p>
     </>
